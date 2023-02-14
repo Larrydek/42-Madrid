1391c06ba03ad6d72df93aa6c43730d25a4c48ea
Juan Manuel De Cleene <jde-clee@c1r12s2.42madrid.com>
2023-02-14 20:13:09 +0100

    strtrim finished | starting split

--- Cursus/Libft/ft_strtrim.c
+++ Cursus/Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jde-clee <jde-clee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 05:25:07 by jde-clee          #+#    #+#             */
-/*   Updated: 2023/02/13 19:02:08 by jde-clee         ###   ########.fr       */
+/*   Updated: 2023/02/14 20:12:30 by jde-clee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,29 @@ char    *ft_strtrim(char const *s1, char const *set)
     size_t i;
     size_t j;
     
+    
     i = 0;
     j = ft_strlen(s1) - 1;
     char *cast_s1 = (char *)s1;
     
-    if (!s1 && !set)
-        return NULL;
-    
+    if (!s1[0])
+        return (ft_strdup("\0"));
+        
     while (ft_strchr(set, s1[i]) != NULL && s1[i])
         i++;
     
     while (ft_strchr(set, s1[j]) != NULL && j > 0)
         j--;
     
-    return (ft_substr(cast_s1, i , j - i + 1));
+    return (ft_substr(cast_s1, i, j - i + 1));
 }
 
-// int main()
-// {
-//     char const *s1 = "SSSSETORETE";
-//     char const *set = "SET";
-//     printf("%s", ft_strtrim(s1, set));
-//     return 0;
-// }
+/* int main()
+{
+    char const *s1 = "";
+    char const *set = "cdef";
+    
+    printf("%s", ft_strtrim(s1, set));
+    
+    return 0;
+} */
