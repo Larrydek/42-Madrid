e342e9daff6d87ec2708a050eb73a9a419055550
Juan Manuel De Cleene <jde-clee@c2r13s5.42madrid.com>
2023-03-09 18:40:09 +0100

    TODO PASA, A PASAR NORMINETTE

--- Cursus/Libft/ft_putnbr_fd.c
+++ Cursus/Libft/ft_putnbr_fd.c
@@ -6,25 +6,29 @@ void ft_putnbr_fd(int n, int fd)
     long num;
     
     num = n;
-    
     if (num < 0)
     {
+
         write(fd, "-", 1);
         num = -num;
     }
-
-    while(num > 0)
+    if(num >= 0)
     {
+        if(n == 0)
+        {
+            write(fd, "0", 1);
+            return;
+        }
         s[0] = (num % 10) + 48;
-        s[1] = 0;
         num = num / 10;
-        ft_putnbr_fd(num, fd);
+        if (num > 0)
+            ft_putnbr_fd(num, fd);
         write(fd, &s[0], 1);
         return;
     }
 }
 
-int main()
+/* int main()
 {
     int n;
     int fd;
@@ -43,4 +47,4 @@ int main()
     write(fd, "\n", 1);
 
     return 0;
-}
+} */
